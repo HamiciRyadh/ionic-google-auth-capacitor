c7c22685e66afe0e97e55456930f03bebfd34a59
m2gi-hamicir <ryadh.hamici@etu.univ-grenoble-alpes.fr>
2022-03-02 16:40:23 +0100

    Added basic working code snippets to handle document creation, updating, retrieving, filtering and observation.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,16 @@
 import {Component} from '@angular/core';
 import {GoogleAuth} from '@codetrix-studio/capacitor-google-auth';
+import {
+  Firestore,
+  collection,
+  doc,
+  setDoc,
+  onSnapshot,
+  where,
+  query,
+  getDocs,
+  updateDoc
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +18,27 @@ import {GoogleAuth} from '@codetrix-studio/capacitor-google-auth';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  userDetails = '';
-  constructor() { }
+  userEmail = '';
+  score = 0;
+  constructor(private db: Firestore) { }
 
   ionViewDidEnter() {
     GoogleAuth.initialize();
+    const users = collection(this.db, 'users');
+
+    // Display the list of users.
+    getDocs(users).then(querySnapshot => {
+      querySnapshot.forEach((document) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(document.id, ' => ', document.data());
+      });
+    });
+
+    // Observe the test user.
+    const docRef = doc(this.db, 'users', 'p5ioeSdGAaoYbZWV3Eja');
+    onSnapshot(docRef, snapshot => {
+      console.log(snapshot.data());
+    });
   }
 
   async doLogin() {
@@ -23,6 +50,40 @@ export class HomePage {
 
   goToHome(user) {
     console.log('User :', user);
-    this.userDetails = user.email;
+    this.userEmail = user.email;
+
+    const q = query(collection(this.db, 'users'), where('email', '==', this.userEmail));
+    getDocs(q).then(querySnapshot => {
+      if (querySnapshot.empty) {
+        // Logged user does not exist, add it to the list of users.
+        setDoc(doc(collection(this.db, 'users'), user.email), {
+          uid: user.id,
+          email: user.email,
+          firstName: user.givenName,
+          lastName: user.familyName,
+          score: 0,
+        }).then(() => this.observeLoggedUserInformation());
+      } else {
+        // Logged user exists, retrieve his information.
+        querySnapshot.forEach((document) => {
+          this.score = document.data().score;
+        });
+        this.observeLoggedUserInformation();
+      }
+    });
+  }
+
+  observeLoggedUserInformation(): void {
+    const docRef = doc(this.db, 'users', this.userEmail);
+    onSnapshot(docRef, snapshot => {
+      this.score = snapshot.data().score;
+    });
+  }
+
+  increaseScore(): void {
+    // Increase the score without modifying the score variable.
+    updateDoc(doc(collection(this.db, 'users'), this.userEmail), {
+      score: this.score+1,
+    });
   }
 }
