988b3a45b88e7cd5749c54933e9b2f62e7296424
HAMICI Ryadh <hamiciryadh@gmail.com>
2022-03-02 12:49:09 +0100

    Added firebase configuration to the project.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBDGTSMwu5EbJOIhn2KgZOjyP8Nmqr_3dA',
+    authDomain: 'login-capacitor-3a7a0.firebaseapp.com',
+    projectId: 'login-capacitor-3a7a0',
+    storageBucket: 'login-capacitor-3a7a0.appspot.com',
+    messagingSenderId: '392755499145',
+    appId: '1:392755499145:web:7759a9d8008a2708d03658',
+    measurementId: 'G-8L3T9410KJ'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBDGTSMwu5EbJOIhn2KgZOjyP8Nmqr_3dA',
+    authDomain: 'login-capacitor-3a7a0.firebaseapp.com',
+    projectId: 'login-capacitor-3a7a0',
+    storageBucket: 'login-capacitor-3a7a0.appspot.com',
+    messagingSenderId: '392755499145',
+    appId: '1:392755499145:web:7759a9d8008a2708d03658',
+    measurementId: 'G-8L3T9410KJ'
+  }
 };
 
 /*
