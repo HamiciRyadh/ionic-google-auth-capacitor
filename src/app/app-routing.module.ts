917013f1c412dd9eeff5c8840573aebf93abf58c
HAMICI Ryadh <hamiciryadh@gmail.com>
2022-03-27 00:47:10 +0100

    Step 3.
    Génération des pages et entités.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,23 @@ const routes: Routes = [
   {
     path: 'profile',
     loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },  {
+    path: 'project',
+    loadChildren: () => import('./pages/project/project.module').then( m => m.ProjectPageModule)
   },
+  {
+    path: 'members',
+    loadChildren: () => import('./pages/members/members.module').then( m => m.MembersPageModule)
+  },
+  {
+    path: 'parameters',
+    loadChildren: () => import('./pages/parameters/parameters.module').then( m => m.ParametersPageModule)
+  },
+  {
+    path: 'ticket',
+    loadChildren: () => import('./pages/ticket/ticket.module').then( m => m.TicketPageModule)
+  },
+
 ];
 
 @NgModule({
