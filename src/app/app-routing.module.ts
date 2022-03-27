6bc630a6497a49decc1dbbd2a348c5cc89d0dade
HAMICI Ryadh <hamiciryadh@gmail.com>
2022-03-28 00:51:57 +0200

    Step 3
    Ajout du AuthGuard et des redirections.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
 const routes: Routes = [
   {
     path: 'home-base',
@@ -8,44 +13,62 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome }
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome }
   },
   {
     path: 'password-recovery',
-    loadChildren: () => import('./pages/password-recovery/password-recovery.module').then( m => m.PasswordRecoveryPageModule)
+    loadChildren: () => import('./pages/password-recovery/password-recovery.module').then( m => m.PasswordRecoveryPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToHome }
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
   {
     path: 'project',
-    loadChildren: () => import('./pages/project/project.module').then( m => m.ProjectPageModule)
+    loadChildren: () => import('./pages/project/project.module').then( m => m.ProjectPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
   {
     path: 'members',
-    loadChildren: () => import('./pages/members/members.module').then( m => m.MembersPageModule)
+    loadChildren: () => import('./pages/members/members.module').then( m => m.MembersPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
   {
     path: 'parameters',
-    loadChildren: () => import('./pages/parameters/parameters.module').then( m => m.ParametersPageModule)
+    loadChildren: () => import('./pages/parameters/parameters.module').then( m => m.ParametersPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
   {
     path: 'ticket',
-    loadChildren: () => import('./pages/ticket/ticket.module').then( m => m.TicketPageModule)
+    loadChildren: () => import('./pages/ticket/ticket.module').then( m => m.TicketPageModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin }
   },
 
 ];
