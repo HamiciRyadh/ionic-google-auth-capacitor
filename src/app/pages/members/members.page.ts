8ea12a9473c9f5472e7b69468928fe03bf1079bc
hamiciryadh <hamiciryadh@gmail.com>
2022-03-30 16:07:39 +0200

    Step 5: Affichage de la liste des membres d'un projet.

--- src/app/pages/members/members.page.ts
+++ src/app/pages/members/members.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
 import {UserService} from '../../services/user.service';
 import {User} from '@firebase/auth';
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProjectService} from "../../services/project.service";
+import {Project} from "../../models/project";
 
 @Component({
   selector: 'app-members',
@@ -13,10 +15,30 @@ export class MembersPage implements OnInit {
   users: User[];
   constructor(private router: Router,
               private route: ActivatedRoute,
-              private userService: UserService) { }
+              private userService: UserService,
+              private projectService: ProjectService) { }
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
+    const projectId = routeParams.get('projectId');
+    const project: Project = this.projectService.getSelectedProject();
+    if (project === undefined || project.id !== projectId) {
+      this.projectService.selectProject(projectId);
+    }
+    this.userService.getObservableUsers().subscribe(users => this.users = users);
   }
 
+  call(user: User): void {}
+
+  addMember(): void {
+  //  TODO: Ajouter un membre en canRead + canWrite
+  }
+
+  addVisitor(): void {
+  //  TODO: Ajouter un membre en canRead seulement
+  }
+
+  removeFromProject(user: User): void {
+  //  TODO: Remove from canRead and canWrite and check for tickets where that member was owner/creator and .. deal with it.
+  }
 }
